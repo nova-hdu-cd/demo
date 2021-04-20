65b29cc39f527302341fcfeda72e39bfacc01a10
chendong <chendong_hdu@163.com>
2021-04-20 22:34:32 +0800

    ++1

--- src/main/java/com/chendong/demo/common/config/MyMvcConfig.java
+++ src/main/java/com/chendong/demo/common/config/MyMvcConfig.java
@@ -1,7 +1,8 @@
 package com.chendong.demo.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,8 +12,32 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 //@EnableWebMvc //这个注解会使得springmvc的自动配置失效,注意配置的favicon失效了
 public class MyMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 添加自己的视图解析器
+     */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/chen").setViewName("index");
+    }
+
+    /**
+     * 添加自己的拦截器
+     *
+     * @param registry
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+
+    }
+
+    /**
+     * 添加自己的格式化
+     *
+     * @param registry
+     */
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+
     }
 }
